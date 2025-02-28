1203650346836a44aaf0481e0899849b5500449e
SupratimDeb01 <supratimdeb04@gmail.com>
2025-02-28 22:52:59 +0530

    Update func.js

--- func.js
+++ func.js
@@ -1,18 +1,34 @@
-document.querySelectorAll('nav a').forEach(anchor => {
+document.querySelectorAll('nav a, .footer a, .popup-navbar a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         if (this.hash !== "") {
             e.preventDefault();
             const target = document.querySelector(this.hash);
-            target.scrollIntoView({ behavior: 'smooth' });
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
         }
     });
 });
-document.querySelectorAll('.footer a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        if (this.hash !== "") {
-            e.preventDefault();
-            const target = document.querySelector(this.hash);
-            target.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
+
+// -------------------------Floating Navbar Functionality-----------------------------
+
+
+// Get elements
+const hamburger = document.getElementById('hamburger-icon');
+const floatingnavbar = document.getElementById('floating-navbar');
+const popupNavbar = document.getElementById('popup-navbar');
+const cancel = document.getElementById('cancel');
+
+// Toggle popup navbar
+hamburger.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+    popupNavbar.style.display="flex";
+    floatingnavbar.style.display="none";
+});
+
+// Close popup navbar
+cancel.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+    popupNavbar.style.display="none";
+    floatingnavbar.style.display="flex";
 });
